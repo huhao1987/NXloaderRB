2331ed47c15af7174dbe6afd1b3e68224b8033c5
Hao <huhao1987@gmail.com>
2018-08-13 12:39:45 +1000

    Finish function of mainactivity: device connection monitor use sx os bin choose custom bin injection

--- app/src/main/java/hh/nxloaderrb/MainActivity.kt
+++ app/src/main/java/hh/nxloaderrb/MainActivity.kt
@@ -1,12 +1,16 @@
 package hh.nxloaderrb
 
+import android.app.PendingIntent
 import android.content.*
+import android.content.pm.PackageManager
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.CompoundButton
+import android.widget.RadioGroup
 import com.github.angads25.filepicker.model.DialogConfigs
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
@@ -29,6 +33,10 @@ import kotlin.collections.ArrayList
 class MainActivity : AppCompatActivity() {
     private val APX_VID = 0x0955
     private val APX_PID = 0x7321
+    private var ACTION_USB_PERMISSION ="hh.USB_PERMISSION"
+    private  var dialog:FilePickerDialog?=null
+    private var usbreceiver:BroadcastReceiver?=null
+    private var useSX=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,58 +46,135 @@ class MainActivity : AppCompatActivity() {
             filepath.text=payload_name
         setSlideMenu()
         setItems()
+        RegisterReceiver()
     }
 
-    fun usbBroadcastreceiver(){
-       var ACTION_USB_PERMISSION ="hh.USB_PERMISSION"
-        var usbreceiver=object:BroadcastReceiver(){
-            override fun onReceive(context: Context?, intent: Intent?) {
-                var action=intent!!.action
-                if(ACTION_USB_PERMISSION.equals(action))
-                {
-                    synchronized(this) {
-                        val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+    override fun onResume() {
+        super.onResume()
+        UsbPermissionCheck()
 
-                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                            device?.apply {
-                                //call method to set up device communication
-                            }
-                        } else {
-                            Log.d(TAG, "permission denied for device $device")
-                        }
-                    } 
-                }
+    }
+
+    fun RegisterReceiver()
+    {
+        if(usbreceiver==null) {
+            usbreceiver = usbBroadcastreceiver()
+            var filter = IntentFilter()
+            filter.addAction(ACTION_USB_PERMISSION)
+            filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+            filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED)
+            registerReceiver(usbreceiver, filter)
+        }
+        else{
+            unregisterReceiver(usbreceiver)
+        }
+    }
+    override fun onDestroy() {
+        if (usbreceiver != null) {
+            unregisterReceiver(usbreceiver)
+
+            super.onDestroy()
+        }
+    }
+    fun UsbPermissionCheck()
+    {
+        var manager =  getSystemService(Context.USB_SERVICE) as UsbManager
+
+        var deviceList = manager.getDeviceList();
+        var  deviceIterator = deviceList.values.iterator();
+        var mPermissionIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+        for(i in manager.deviceList)
+        {
+            var device=i.value
+            if(manager.hasPermission(device))
+            {
+                switchstatus.text=getString(R.string.deviceconnection)
+                switchstatus.setTextColor(resources.getColor(R.color.light_green))
+            }
+            else {
+                switchstatus.text=getString(R.string.devicenotconnection)
+                switchstatus.setTextColor(resources.getColor(R.color.red))
+                manager.requestPermission(device, mPermissionIntent)
             }
 
         }
     }
+
+//    File permission check
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode)
+        {
+            FilePickerDialog.EXTERNAL_READ_PERMISSION_GRANT->{
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(dialog!=null)
+                    {
+                        //Show dialog if the read permission has been granted.
+                        dialog!!.show();
+                    }
+                }
+                else {
+                    //Permission has not been granted. Notify the user.
+                    dialog!!.dismiss()
+                }
+            }
+        }
+    }
+
+//    USB broadcasterreceiver
+    fun usbBroadcastreceiver():BroadcastReceiver{
+        var usbreceiver=object:BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                var action=intent!!.action
+                when(action)
+                {
+                    ACTION_USB_PERMISSION->{
+                        synchronized(this) {
+                            val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+
+                            if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                                device?.apply {
+                                    //call method to set up device communication
+
+                                }
+                            } else {
+
+                            }
+                        }
+                    }
+                    UsbManager.ACTION_USB_DEVICE_ATTACHED->
+                        UsbPermissionCheck()
+
+                    UsbManager.ACTION_USB_DEVICE_DETACHED->
+                    {
+                        switchstatus.text=getString(R.string.devicenotconnection)
+                        switchstatus.setTextColor(resources.getColor(R.color.red))
+                    }
+                }
+
+            }
+
+        }
+        return usbreceiver
+    }
+
+//    set the slide menu
     fun setSlideMenu()
     {
         var menuarray=resources.getStringArray(R.array.menu)
-        val item1 = SecondaryDrawerItem().withIdentifier(1).withName(menuarray.get(0)).withSelectable(false)
-        val item2 = SecondaryDrawerItem().withIdentifier(2).withName(menuarray.get(1)).withSelectable(false)
-        val item3 = SecondaryDrawerItem().withIdentifier(3).withName(menuarray.get(2)).withSelectable(false)
-
-
+        val item1 = SecondaryDrawerItem().withIdentifier(1).withName(menuarray.get(1)).withSelectable(false)
+        toolbar.title=menuarray.get(0)
         var leftmenu= DrawerBuilder()
                 .withActivity(this@MainActivity)
                 .addDrawerItems(
-                        item1,
-                        item2,
-                        item3
+                        item1
                 )
                 .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener{
                     override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*, *>?): Boolean {
                         when(drawerItem!!.identifier.toInt())
                         {
                             1->{
-
-                            }
-                            2->{
-
-                            }
-                            3->{
-
+                                startActivity(Intent(this@MainActivity,AboutActivity::class.java))
                             }
 
                         }
@@ -101,32 +186,58 @@ class MainActivity : AppCompatActivity() {
                 .build()
     }
 
+//    Init the elements and functions
     fun setItems()
     {
         val properties = DialogProperties()
-        properties.selection_mode = DialogConfigs.SINGLE_MODE;
-        properties.selection_type = DialogConfigs.FILE_SELECT;
-        properties.root = File(DialogConfigs.DEFAULT_DIR);
-        properties.error_dir = File(DialogConfigs.DEFAULT_DIR);
-        properties.offset = File(DialogConfigs.DEFAULT_DIR);
+        properties.selection_mode = DialogConfigs.SINGLE_MODE
+        properties.selection_type = DialogConfigs.FILE_SELECT
+        properties.root = File(DialogConfigs.DEFAULT_DIR)
+        properties.error_dir = File(DialogConfigs.DEFAULT_DIR)
+        properties.offset = File(DialogConfigs.DEFAULT_DIR)
         var binextension=ArrayList<String>()
         binextension.add("bin")
-        properties.extensions =binextension.toTypedArray() ;
-        val dialog = FilePickerDialog(this@MainActivity, properties)
-        dialog.setTitle(getString(R.string.fileselmsg))
-        dialog.setDialogSelectionListener (object:DialogSelectionListener{
+        properties.extensions =binextension.toTypedArray()
+        var sharepreferences=getSharedPreferences("Config", Context.MODE_PRIVATE)
+        useSX = sharepreferences.getBoolean("useSX", false)
+        if(useSX)setsxosswitch.isChecked=useSX
+        setsxosswitch.setOnCheckedChangeListener(object:CompoundButton.OnCheckedChangeListener{
+            override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+                var sharepreferences=getSharedPreferences("Config", Context.MODE_PRIVATE)
+                var shareditor:SharedPreferences.Editor?=null
+                if(isChecked)
+                {
+                    shareditor=sharepreferences.edit().putBoolean("useSX",true)
+                    filebtn.isClickable=false
+                    filepath.text=getString(R.string.sxossetdes)
+                    fileselection.setBackgroundColor(resources.getColor(R.color.gray))
+                }
+                else{
+                    shareditor=sharepreferences.edit().putBoolean("useSX",false)
+                    filebtn.isClickable=true
+                    var payload_name = sharepreferences.getString("binpath", null)
+                    filepath.text=payload_name
+                    fileselection.setBackgroundColor(resources.getColor(R.color.white))
+
+                }
+                shareditor!!.apply()
+            }
+        })
+        dialog = FilePickerDialog(this@MainActivity, properties)
+        dialog!!.setTitle(getString(R.string.fileselmsg))
+        dialog!!.setPositiveBtnName(getString(R.string.filesepo))
+        dialog!!.setNegativeBtnName(getString(R.string.filesena))
+        dialog!!.setDialogSelectionListener (object:DialogSelectionListener{
             override fun onSelectedFilePaths(files: Array<out String>?) {
                 filepath.text=files!!.get(0)
                 var sharepreferences=getSharedPreferences("Config", Context.MODE_PRIVATE)
                 var shareditor=sharepreferences.edit().putString("binpath",files!!.get(0))
                 shareditor.apply()
             }
-
         })
         filebtn.setOnClickListener {
-         dialog.show()
+         dialog!!.show()
         }
-
         injection.setOnClickListener {
             val intent = intent
             var usbManager=getSystemService(Context.USB_SERVICE) as UsbManager
@@ -135,14 +246,11 @@ class MainActivity : AppCompatActivity() {
             for(a in devicelist)
             {
                 if(a.value.productId==APX_PID&&a.value.vendorId==APX_VID) {
-                    var u=USBprocess()
-                    u.init()
-                    u.handleDevice(this,a.value)
+                    var u=PrimaryLoader()
+                    if(u!=null)
+                    u.handleDevice(this,a.value,useSX)
                 }
-
             }
         }
     }
-
-
 }
---
+++ app/src/main/java/hh/nxloaderrb/PrimaryLoader.kt
@@ -0,0 +1,157 @@
+package hh.nxloaderrb
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbEndpoint
+import android.hardware.usb.UsbInterface
+import android.hardware.usb.UsbManager
+import android.util.Log
+
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+//Base on https://github.com/DavidBuchanan314/NXLoader
+//Rewrite by kotlin
+
+class PrimaryLoader  {
+    private val TAG = "ConnectTag"
+    fun handleDevice(context: Context, device: UsbDevice,useSX:Boolean=false) {
+        Log.d(TAG, "[+] Launching primary payload!!!")
+
+        val mUsbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
+        val intf = device.getInterface(0)
+        val endpoint_in = intf.getEndpoint(0)
+        val endpoint_out = intf.getEndpoint(1)
+        val conn = mUsbManager.openDevice(device)
+        conn.claimInterface(intf, true)
+
+        /* Step 1: Read device ID */
+
+        val deviceID = ByteArray(16)
+        if (conn.bulkTransfer(endpoint_in, deviceID, deviceID.size, 999) != deviceID.size) {
+            Log.d(TAG, "[-] Failed to read device ID, bailing out :(")
+            return
+        }
+
+        Log.d(TAG, "[+] Read device ID: " + Utils.bytesToHex(deviceID))
+
+        /* Step 2: Start building payload */
+
+        val payload = ByteBuffer.allocate(MAX_LENGTH)
+        payload.order(ByteOrder.LITTLE_ENDIAN)
+
+        payload.putInt(MAX_LENGTH)
+        payload.put(ByteArray(676))
+
+        // smash the stack with the address of the intermezzo
+        var i = RCM_PAYLOAD_ADDR
+        while (i < INTERMEZZO_LOCATION) {
+            payload.putInt(INTERMEZZO_LOCATION)
+            i += 4
+        }
+
+        val intermezzo: ByteArray
+        try {
+            val intermezzoStream = context.assets.open("intermezzo.bin")
+            intermezzo = ByteArray(intermezzoStream.available())
+            intermezzoStream.read(intermezzo)
+            intermezzoStream.close()
+        } catch (e: IOException) {
+            Log.d(TAG, "[-] Failed to read intermezzo: " + e.toString())
+            return
+        }
+
+        payload.put(intermezzo)
+
+        // pad until payload
+        payload.put(ByteArray(PAYLOAD_LOAD_BLOCK - INTERMEZZO_LOCATION - intermezzo.size))
+
+        // write the actual payload file
+        try {
+            payload.put(getPayload(context,useSX))
+        } catch (e: IOException) {
+            Log.d(TAG, "[-] Failed to read payload: " + e.toString())
+            return
+        }
+
+        val unpadded_length = payload.position()
+        payload.position(0)
+        // always end on a high buffer
+        var low_buffer = true
+        val chunk = ByteArray(0x1000)
+        var bytes_sent: Int
+        bytes_sent = 0
+        while (bytes_sent < unpadded_length || low_buffer) {
+            payload.get(chunk)
+            if (conn.bulkTransfer(endpoint_out, chunk, chunk.size, 999) != chunk.size) {
+                Log.d(TAG, "[-] Sending payload failed at offset " + Integer.toString(bytes_sent))
+                return
+            }
+            low_buffer = low_buffer xor true
+            bytes_sent += 0x1000
+        }
+
+        Log.d(TAG, "[+] Sent " + Integer.toString(bytes_sent) + " bytes")
+
+        // 0x7000 = STACK_END = high DMA buffer address
+        when (nativeTriggerExploit(conn.fileDescriptor, 0x7000)) {
+            0 -> Log.d(TAG, "[+] Exploit triggered!")
+            -1 -> Log.d(TAG, "[-] SUBMITURB failed :(")
+            -2 -> Log.d(TAG, "[-] DISCARDURB failed :(")
+            -3 -> Log.d(TAG, "[-] REAPURB failed :(")
+            -4 -> Log.d(TAG, "[-] Wrong URB reaped :( Maybe that doesn't matter?")
+            else -> {
+                Log.d(TAG, "[-] How did you get here!?")
+                return
+            }
+        }
+
+        conn.releaseInterface(intf)
+        conn.close()
+    }
+
+    @Throws(IOException::class)
+    private fun getPayload(context: Context,useSX:Boolean): ByteArray {
+        val sharepreferences = context.getSharedPreferences("Config", Context.MODE_PRIVATE)
+        val payload_name = sharepreferences.getString("binpath", null)
+        var payload_file: InputStream
+        if(useSX)
+            payload_file = context.assets.open("SXOS.bin")
+        else {
+            if (payload_name == null) {
+                Log.d(TAG, "[*] Opening SXOS payload")
+               payload_file = context.assets.open("fusee.bin")
+            } else {
+                Log.d(TAG, "[*] Opening custom payload ($payload_name)")
+                payload_file = FileInputStream(payload_name)
+            }
+        }
+        val payload_data = ByteArray(payload_file.available())
+        Log.d(TAG, "[+] Read " + Integer.toString(payload_file.read(payload_data)) + " bytes from payload file")
+        payload_file.close()
+        return payload_data
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun nativeTriggerExploit(fd: Int, length: Int): Int
+
+    companion object {
+        private val RCM_PAYLOAD_ADDR = 0x40010000
+        private val INTERMEZZO_LOCATION = 0x4001F000
+        private val PAYLOAD_LOAD_BLOCK = 0x40020000
+        private val MAX_LENGTH = 0x30298
+
+        // Used to load the 'native-lib' library on startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
